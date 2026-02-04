48142fad9ad69120c7bac0d21267915d6d2f4f4f
ReCoL1337 <nataniel2003@gmail.com>
2026-02-04 16:54:21 +0100

    mm

--- lua/recol/lazy/lsp.lua
+++ lua/recol/lazy/lsp.lua
@@ -1,3 +1,9 @@
+vim.filetype.add({
+    extension = {
+        tf = "terraform"
+    }
+})
+
 return {
     {
         'VonHeikemen/lsp-zero.nvim',
@@ -72,8 +78,8 @@ return {
                 vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
                 vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
                 vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-                vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-                vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+                vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
+                vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
                 vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
                 vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
                 vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
@@ -83,6 +89,7 @@ return {
 
             require('mason-lspconfig').setup({
                 ensure_installed = {
+                    'terraformls',
                     'ansiblels',
                     'clangd',
                     'dockerls',
